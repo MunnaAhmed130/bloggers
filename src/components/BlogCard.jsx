781b40c81512e53106200532f94973c3aa8c5ecc
Munna Ahmed <munnaahmed2025@gmail.com>
2023-01-31 00:46:35 +0600

    blog page

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const BlogCard = ({ blog }) => {
     const tags = blog.blog.tags;
     console.log(blog.blog.tags);
     // console.log(data);
-    tags.map((tag) => console.log(tag));
+    // ${tag === "Science" && "bg-blue-700"}
+    // let bgColor;
+    // tags.map((tag) => {
+    //     console.log(tag);
+    // });
     return (
         <div className={`border rounded-lg overflow-hidden  `}>
+            <Link to={`/blog/${blog.blog.title}`}>
+                <img
+                    className="h-48 w-full bg-cover border-0"
+                    src={blog.blog.image}
+                    alt=""
+                />
+            </Link>
             {/* <div className=""> */}
-            <img
-                className="h-48 w-full bg-cover border-0"
-                src={blog.blog.image}
-                alt=""
-            />
+
             <div className="px-3">
                 <div className="flex items-center gap-5 pt-3 ">
                     {/* <img
@@ -21,26 +29,35 @@ const BlogCard = ({ blog }) => {
                         alt=""
                     /> */}
                     <div className="flex flex-col">
-                        <h2 className="text-md font-semibold">
-                            {blog.blog.title}
-                        </h2>
+                        <Link to="">
+                            <h2 className="text-md font-semibold">
+                                {blog.blog.title}
+                            </h2>
+                        </Link>
+                        <Link to="">
+                            <h4 className="text-sm  text-gray-900">
+                                by {blog.author.name}
+                            </h4>
+                        </Link>
 
-                        <h4 className="text-sm  text-gray-900">
-                            by {blog.author.name}
-                        </h4>
                         {/* <span className="text-sm text-gray-500">
                             {blog.blog.publishedDate}
                         </span> */}
                         {/* <p className="text-sm">{blog.author.name}</p> */}
                     </div>
                 </div>
-
                 <div>
                     {tags.map((tag) => {
                         return (
                             <button
-                                className="text-xxs font-semibold mr-1 mt-1 px-1 py-0.5 bg-red-500 rounded-sm text-white"
+                                className={`
+                                ${tag === "Science" && "bg-blue-600"}
+                                ${tag === "Technology" && "bg-blue-800"}
+                                ${tag === "Health" && "bg-green-800"}
+                                ${tag === "Space" && "bg-orange-500"}
+                                 text-xxs font-semibold mr-1 mt-1 px-1 py-0.5  rounded-sm text-white`}
                                 key={tag}
+                                onClick={() => console.log(tag)}
                             >
                                 {tag}
                             </button>
@@ -48,6 +65,7 @@ const BlogCard = ({ blog }) => {
                     })}
                 </div>
                 <p className="text-sm text-gray-500 py-2 ">
+                    {/* {blog.blog.description} */}
                     {blog.blog.description.slice(0, 200)}...
                     <span className="font-semibold text-gray-400">
                         see more
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main/Main";
+import Blog from "../pages/Main/Blog";
 import Home from "../pages/Main/Home";
 import ReadingHistory from "../pages/Main/ReadingHistory";
 
@@ -15,6 +16,10 @@ const routes = createBrowserRouter([
             {
                 path: "/history",
                 element: <ReadingHistory />,
+            },
+            {
+                path: "/blog/:id",
+                element: <Blog />,
             },
         ],
     },
