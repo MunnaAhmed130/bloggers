6fcf293711369eaf4b4644791c5d543ac6363f32
Munna Ahmed <munnaahmed2025@gmail.com>
2023-02-08 22:20:07 +0600

    change in authorinfo component

--- src/components/AuthorInfo.jsx
+++ src/components/AuthorInfo.jsx
@@ -4,7 +4,7 @@ import { ImLink } from "react-icons/im";
 import { RxEnvelopeClosed } from "react-icons/rx";
 
 const AuthorInfo = ({ author }) => {
-    const iconStyle = "mr-3.5";
+    const iconStyle = "mr-2 py-1.5 px-1";
     return (
         <div className=" mx-8 py-10 font-sans sticky top-0">
             <div className="flex">
@@ -15,19 +15,35 @@ const AuthorInfo = ({ author }) => {
                 />
                 <div className="pl-5  w-full flex flex-col justify-center">
                     <h2 className="font-medium text-lg">{author.name}</h2>
-                    <span className="text-gray-400 ">
-                        {author.followersCount} followers
+                    <span>
+                        <a
+                            href="/"
+                            className="text-gray-400 hover:text-gray-500"
+                        >
+                            {author.followersCount} followers
+                        </a>
                     </span>
-                    <div className="flex flex-row my-2 text-gray-500">
-                        <BsTwitter className={iconStyle} />
-                        <BsFacebook className={iconStyle} />
-                        <BsLinkedin className={iconStyle} />
-                        <ImLink className={iconStyle} />
-                        <BsThreeDots className={iconStyle} />
+
+                    <div className="flex flex-row  text-slate-500">
+                        <button className={iconStyle}>
+                            <BsTwitter />
+                        </button>
+                        <button className={iconStyle}>
+                            <BsFacebook />
+                        </button>
+                        <button className={iconStyle}>
+                            <BsLinkedin />
+                        </button>
+                        <button className={iconStyle}>
+                            <ImLink />
+                        </button>
+                        <button className={iconStyle}>
+                            <BsThreeDots />
+                        </button>
                     </div>
                 </div>
             </div>
-            <p className="mt-5 text-gray-400">{author.about}</p>
+            <p className="mt-5 text-gray-500 text-sm">{author.about}</p>
             <div className="flex mt-5">
                 <button className="px-5 py-2 rounded-full bg-gray-800 text-white mr-2">
                     Follow
